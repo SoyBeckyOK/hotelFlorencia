7b2e76d99f5b7650084f3e710c891be0632ec353
SoyBeckyOK <rebecasarai_gb@hotmail.com>
2019-01-07 12:03:32 -0600

    Restaurante progresando

--- hotelFlorencia/hotelFlorencia/View/RestaurantePage.xaml.cs
+++ hotelFlorencia/hotelFlorencia/View/RestaurantePage.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using hotelFlorencia.View.Restaurante;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -23,9 +23,44 @@ namespace hotelFlorencia.View
 
         }
 
-        private void Button_Clicked_1()
+        private async void Button_Clicked_1(object sender, EventArgs e)
         {
+            await App.Current.MainPage.Navigation.PushAsync(new EntradasPage());
+        }
 
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new CevichesPage());
+        }
+
+        private async void Button_Clicked_3(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new CarnesPage());
+        }
+
+        private async void Button_Clicked_4(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new MariscosPage());
+        }
+
+        private async void Button_Clicked_5(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new CortesEspecialesPage());
+        }
+
+        private async void Button_Clicked_6(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new ParrilladaPage());
+        }
+
+        private async void Button_Clicked_7(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new BebidasPage());
+        }
+
+        private async void Button_Clicked_8(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new PostresPage());
         }
     }
 }
